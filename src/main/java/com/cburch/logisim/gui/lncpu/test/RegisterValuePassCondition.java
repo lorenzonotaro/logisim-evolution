8587f4bd9ce61914c76da179b531c4361518ae3f
Lorenzo Notaro <lorenzo.notaro1@gmail.com>
2024-07-19 21:28:31 +0200

    3.9.9: fix TestPassCondition .toString()

--- src/main/java/com/cburch/logisim/gui/lncpu/test/RegisterValuePassCondition.java
+++ src/main/java/com/cburch/logisim/gui/lncpu/test/RegisterValuePassCondition.java
@@ -27,6 +27,6 @@ public class RegisterValuePassCondition implements IPassCondition{
 
     @Override
     public String toString() {
-        return signal.displayName + " = " + value;
+        return "%s = 0x%02x".formatted(signal.displayName, value);
     }
 }
